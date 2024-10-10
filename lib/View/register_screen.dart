93bf20346841d34bbdb620c00ffd7df1b75104e5
Laura Pivoto <laurapivoto1@gmail.com>
2024-10-09 22:35:39 -0300

    add register screen components

--- lib/View/register_screen.dart
+++ lib/View/register_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,10 +10,15 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Icon(
             Icons.monetization_on,
@@ -22,8 +29,21 @@ class _RegisterPageState extends State<RegisterPage> {
             style: TextStyle(fontSize: 20),
           ),
           Text('Olá, crie sua conta preenchendo os campos abaixo:'),
-          TextField(),
-          TextField()
+          TextField(
+            controller: _nameController,
+            decoration: InputDecoration(hintText: 'Coloque seu nome aqui'),
+          ),
+          TextField(
+            controller: _emailController,
+            decoration: InputDecoration(hintText: 'Coloque seu e-mail aqui'),
+          ),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(hintText: 'Coloque sua senha aqui'),
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Cadastrar')),
+          Text('Já possui login?'),
+          ElevatedButton(onPressed: () {}, child: Text('Fazer login'))
         ],
       ),
     );
