43b37d0ce2b43c3b38bf003bc211d10083ca669b
Laura Pivoto <laurapivoto1@gmail.com>
2024-10-09 22:10:34 -0300

    create project

---
+++ android/app/src/main/kotlin/com/example/projeto_c214/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projeto_c214
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      routes: {'/': (context) => RegisterPage()},
+      initialRoute: '/',
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
