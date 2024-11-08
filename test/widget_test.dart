619eb3c3d3741df4c566d00f4eb8853bdee39b7b
Álvaro Lúcio Almeida Ribeiro <alvaroluciorib@gmail.com>
2024-11-08 16:59:15 -0300

    tests(widget_test): Create widget test
    A widget test for the registration
    screen was created.

---
+++ test/widget_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:projeto_c214/View/login_screen.dart';
+import 'package:projeto_c214/View/pie_chart.dart';
+import 'package:projeto_c214/View/register_screen.dart';
+
+void main() {
+  group('Widget Tests for RegisterPage', () {
+    testWidgets('should display the registration form with inputs and button',
+        (WidgetTester tester) async {
+      // Sets a larger screen height to prevent overflow
+      await tester.pumpWidget(
+        MaterialApp(
+          home: MediaQuery(
+            data: const MediaQueryData(size: Size(800, 1400)),
+            child: RegisterPage(),
+          ),
+        ),
+      );
+
+      // Checks if text fields and the button are present
+      expect(find.text('XicoCoin'), findsOneWidget);
+      expect(find.text('Olá, crie sua conta preenchendo os campos abaixo:'),
+          findsOneWidget);
+      expect(find.text('Cadastrar'), findsOneWidget);
+      expect(find.byType(TextField), findsNWidgets(3));
+    });
+  });
+}
